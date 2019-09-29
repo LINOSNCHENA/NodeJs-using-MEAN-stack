a363cf257d9565b07ad8f2ecfb6adca4cc517cf0
Linos Nchena <42037657+LINOSNCHENA@users.noreply.github.com>
2019-09-29 23:50:22 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ var express = require('express'),
 mongoose.Promise = global.Promise;
 mongoose.set('useNewUrlParser', true);
 mongoose.connect('mongodb://localhost/Transact_MSQL'); 
-app.use(bodyParser.urlencoded({ extended: true }));  // Auxiliary #1
+app.use(bodyParser.urlencoded({ extended: true }));    // Auxiliary #1
 app.use(bodyParser.json());
 var routes = require('./api/services/wServices');      // Auxiliary #2
 routes(app);                               
-app.use(function(req, res) {                          // Auxiliary #3
+app.use(function(req, res) {                           // Auxiliary #3
   res.status(404).send({url: req.originalUrl + ' possibly this is not correct network address'})
 })
 app.listen(port);
