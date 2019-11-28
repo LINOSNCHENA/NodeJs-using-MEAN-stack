9d7e829c5ac34a3020cdf09e2805e8a749d64af2
linos nchena <linosnchena@gmail.com>
2019-11-28 21:52:10 +0100

    Mongoose connection edited for new style

---
+++ api/services/Services.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = function(app) {
+  var actionJobs = require('../controller/Controller');
+                              
+  
+  // the five services offered
+  
+  app.route('/employees')
+    .get(actionJobs.listEmployees)
+    .post(actionJobs.addEmployee);
+
+  app.route('/employee/:taskId')
+    .get(actionJobs.showEmployee)
+    .put(actionJobs.updateEmployee)
+    .delete(actionJobs.deleteEmployee);
+};
--- server.js
+++ server.js
@@ -2,18 +2,24 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 7070,
   mongoose = require('mongoose'),
-  Task = require('./api/models/wEmployee'), 
+  Task = require('./api/models/Muntu'), 
   bodyParser = require('body-parser');
-// mongoose instance connection url connection
+
+// New style for setting connection to MongoDB
 mongoose.Promise = global.Promise;
-mongoose.set('useNewUrlParser', true);
-mongoose.connect('mongodb://localhost/Transact_MSQL'); 
-app.use(bodyParser.urlencoded({ extended: true }));    // Auxiliary #1
+mongoose.connect("mongodb://localhost/presly", 
+{useUnifiedTopology: true,
+ useNewUrlParser: true, 
+ useCreateIndex: true });
+
+// Other things here!
+app.use(bodyParser.urlencoded({ extended: true }));     // Auxiliary #1
 app.use(bodyParser.json());
-var routes = require('./api/services/wServices');      // Auxiliary #2
+var routes = require('./api/services/Services');        // Auxiliary #2
 routes(app);                               
-app.use(function(req, res) {                           // Auxiliary #3
-  res.status(404).send({url: req.originalUrl + ' possibly this is not correct network address'})
-})
+app.use(function(req, res) {                           
+  res.status(404).send({url: req.originalUrl + 
+    ' possibly this is not correct URL address'})})     // Auxiliary #3
+
 app.listen(port);
 console.log('Transactions API server started on: ' + port);
