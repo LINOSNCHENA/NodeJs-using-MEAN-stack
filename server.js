2aa9f2f4db6c7891d9e42e7d9c05df692bb4783a
linos nchena <linosnchena@gmail.com>
2019-11-28 22:12:36 +0100

    Mongoose connection edited for new style

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 7070,
+  port = process.env.PORT || 4040,
   mongoose = require('mongoose'),
-  Task = require('./api/models/Muntu'), 
+  Task = require('./RESTFUL_API/models/Muntu'), 
   bodyParser = require('body-parser');
 
 // New style for setting connection to MongoDB
@@ -15,11 +15,11 @@ mongoose.connect("mongodb://localhost/presly",
 // Other things here!
 app.use(bodyParser.urlencoded({ extended: true }));     // Auxiliary #1
 app.use(bodyParser.json());
-var routes = require('./api/services/Services');        // Auxiliary #2
+var routes = require('./RESTFUL_API/services/Services');        // Auxiliary #2
 routes(app);                               
 app.use(function(req, res) {                           
   res.status(404).send({url: req.originalUrl + 
     ' possibly this is not correct URL address'})})     // Auxiliary #3
 
 app.listen(port);
-console.log('Transactions API server started on: ' + port);
+console.log('Restful-Backend API server started on: ' + port);
