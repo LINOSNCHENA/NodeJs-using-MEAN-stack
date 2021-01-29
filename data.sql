3288e08699a712cbbfb115bd7408f3c432b036fc
Linos Nchena <linosnchena@gmail.com>
2021-01-29 07:14:53 +0100

    gitignore

--- data.sql
+++ data.sql
@@ -3,4 +3,17 @@
 "post" :"post",
 "salary" :344,
 "status":"permanent"
+},
+{ "name" :"name",
+"dept" :"dept",
+"post" :"post",
+"salary" :344,
+"status":"permanent"
+}
+,
+{ "name" :"name",
+"dept" :"dept",
+"post" :"post",
+"salary" :344,
+"status":"permanent"
 }
